92f88807bc1b0e37bb4faf950b26055d3b7d028e
Thau26 <thauanyaraujoalves@hotmail.com>
2020-08-07 12:50:18 -0300

    listagem com filtros

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,32 +2,54 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+
+
+export interface Teacher{
+        id: number;
+        name: string;
+        avatar: string;
+        bio: string;
+        whatsapp: string;
+        subject: string;
+        cost: number;
+
+}
+
+interface TeacherProps {
+    teacher: Teacher
+}
+
+
+const TeacherItem:React.FC<TeacherProps> = ({teacher})=> {
+
+    function createNewConnection(){
+        const id = teacher.id;
+        api.post('/connections', {
+            user_id: id,
+        } );
+    }
+
     return(
         <article className="teacher-item">
                     <header>
-                        <img src="https://avatars0.githubusercontent.com/u/20343587?s=460&u=a433162b215245b280e9a3259c303e5e70062333&v=4" alt=""/>
+                        <img src={teacher.avatar} alt={teacher.name} />
                         <div>
-                            <strong>Thauny Araujo</strong>
-                            <span>Matemática</span>
+                            <strong>{teacher.name}</strong>
+                            <span>{teacher.subject}</span>
                         </div>
                     </header>
-                    <p>
-                        Entusiasta por fazer calculos matemáticos e provar que 2 + 2 = 4!!
-                        <br/>
-                        <br/>
-                        Apaixonada por fazer cálcuos matemáticos ..daskjhfjwge mudar a vida das pessoas ...,sajhfg
-                    </p>
+                    <p>{teacher.bio}                    </p>
                     <footer>
                         <p>
                             Preço/Hora
-                            <strong>R$ 80,00 </strong>
+                            <strong>R$  {teacher.cost}</strong>
                         </p>
-                        <button type="button">
+                        <a  onClick={createNewConnection} target="_blank" href={`https://wa.me/${teacher.whatsapp}`} >
                             <img src={whatsappIcon} alt="whatsapp"/>
                             Entrar em contato
-                        </button>
+                        </a>
 
                     </footer>
 
